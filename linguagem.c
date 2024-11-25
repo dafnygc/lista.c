bba34fa898975afa4c659ebb78173bf085486ce8
dafnygc <129951774+dafnygc@users.noreply.github.com>
2024-11-25 12:29:55 -0300

    Update linguagem.c

--- linguagem.c
+++ linguagem.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+//imprime cabecalho do nosso jogo
 int main(){
-    printf("**************************************\n");
+    printf("*******************************-********\n");
     printf("*Bem vindo ao nosso jogo de adivinhação*\n");  
-    printf("***************************************\n");
+    printf("****************************************\n");
+
+   int numerosecreto = 42;
+
+   int chute;
+
+   printf("Qual é o seu chute?");
+   scanf("%d"\,&chute);
+   printf("seu chute foi ")
+
+   
+
 }
